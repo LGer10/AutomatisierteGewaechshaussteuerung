ba3eef7a33191836ac653547059be2ae0ae44458
Kilian Soltermann <kilian.soltermann@gmx.ch>
2021-03-09 22:29:52 +0100

    Neu

---
+++ webserver/boot.py
@@ -0,0 +1,19 @@
+def connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('OBI_WLAN_KENOBI', 'Ithurtswhenip')
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
+def no_debug():
+    import esp
+    # this can be run from the REPL as well
+    esp.osdebug(None)
+
+
+no_debug()
+connect()
