77494a14afff1b273eafed482267f3deabf8f9ad
Lars Gerber <lge051198@iet-gibb.ch>
2021-03-22 00:42:59 +0100

    commit

--- flask_app/app.py
+++ flask_app/app.py
@@ -171,7 +171,7 @@ def admin():
 
             return redirect(url_for('admin'))
 
-    return render_template('admin.html', satellite_list=satellite_list, programm_list=programm_list, satellite_name=satellite_name, programm_name=programm_name)
+    return render_template('admin.html', satellite_list=satellite_list, programm_list=programm_list)
 
 
 @app.route('/test')
