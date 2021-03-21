faee64d0d37b2af8dcd3d0268adbb3e85eed36df
Lars Gerber <lge051198@iet-gibb.ch>
2021-03-21 20:30:35 +0100

    commit

--- flask_app/app.py
+++ flask_app/app.py
@@ -118,6 +118,8 @@ def admin():
 
                         for index in range(len(programm_name_list)):
                                 programm_name_array.append(programm_name_list[index][0])
+
+                        for itme in programm_name_array:
                                 programm_id = cur.execute('select id from programms where name = (%s)', [programm_name_array[index]])
                                 cur.execute('insert into satellite_programm (id_satellite, id_programm) VALUES ((%s), (%s))', [satellite_id, programm_id])
                                 mysql.connection.commit()
