f5a1a956a89c792a8407ef54e772a51515dde827
Lars Gerber <lge051198@iet-gibb.ch>
2021-03-29 18:43:34 +0200

    formatted

--- Steuereinheit/Collector/data_collector.py
+++ Steuereinheit/Collector/data_collector.py
@@ -40,31 +40,32 @@ def collector():
         for satellite in satellite_array:
 
             # Aktuelle geladenes Programm abfragen
-    cursor.execute(
-        'SELECT current_programm FROM satellites WHERE ip_addr = (%s)', satellit)
-    current_programm = cursor.fetchone()
-    # REST-API anfragen
+        cursor.execute(
+            'SELECT current_programm FROM satellites WHERE ip_addr = (%s)', satellit)
+        current_programm = cursor.fetchone()
+        # REST-API anfragen
 
-    response = requests.get("http://" + '%s' + ":8081/get_data", [satellite])
-    json_file = json.loads(response.text)
-    print(json_file)
+        response = requests.get(
+            "http://" + '%s' + ":8081/get_data", [satellite])
+        json_file = json.loads(response.text)
+        print(json_file)
 
-    temperature = json_file["temperature"]
-    brightness_hours = json_file["brightness_hours"]
-    soil_humidity = json_file["Soil Humidity"]
-    air_humidity = json_file["air_humidity"]
+        temperature = json_file["temperature"]
+        brightness_hours = json_file["brightness_hours"]
+        soil_humidity = json_file["Soil Humidity"]
+        air_humidity = json_file["air_humidity"]
 
-    cursor.execute('''SELECT id from satellite_programm where id_satellite in 
-    (select id from satellites where ip_addr = (%s) and current_programm = (%s))''', [satellit, current_programm])
+        cursor.execute('''SELECT id from satellite_programm where id_satellite in 
+        (select id from satellites where ip_addr = (%s) and current_programm = (%s))''', [satellit, current_programm])
 
-    id_satellite_programm = cursor.fetchone()
+        id_satellite_programm = cursor.fetchone()
 
-    cursor.execute('''INSERT INTO sensordata 
-    (id_satellite_programm, date, time, temperature, brightness, airhumidity, soilhumidity) 
-    VALUES (%s, current_date(), current_time(), %s, %s)''', [id_satellite_programm, temperature, brightness, air_humidity, soil_humidity])
+        cursor.execute('''INSERT INTO sensordata 
+        (id_satellite_programm, date, time, temperature, brightness, airhumidity, soilhumidity) 
+        VALUES (%s, current_date(), current_time(), %s, %s)''', [id_satellite_programm, temperature, brightness, air_humidity, soil_humidity])
 
-    mysql.connection.commit()
-    cursor.close()
+        mysql.connection.commit()
+        cursor.close()
 
     #cursor.execute("insert into sensordata (date, time, id_satellite_programm, temperature, airhumidity) values (%s, %s, '1', %s, %s), (current_date, current_time, temperature, air_humidity)")
     #cursor.execute("INSERT INTO sensordata (date, time, id_satellite_programm, temperature, airhumidity) VALUES (%s, %s, 1, %s, %s)", current_date, current_time, temperature, air_humidity)
