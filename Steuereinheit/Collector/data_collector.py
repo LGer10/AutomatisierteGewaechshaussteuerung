ce7fd66a0dbf5a7579278b87f804d07c6f71da33
Lars Gerber <lge051198@iet-gibb.ch>
2021-03-29 18:42:47 +0200

    fix sql connection

--- Steuereinheit/Collector/data_collector.py
+++ Steuereinheit/Collector/data_collector.py
@@ -15,16 +15,15 @@ try:
     connection = mysql.connector.connect(
         host="localhost", user="root", passwd="AGdb", db="AGdb")
     #print("Verbunden mit SQL Server")
-    cursor = connection.cursor()
 
 except:
     print("Keine Verbindung zum Server")
     sys.exit(0)
 
 # Satelliten aus DB auslesen
+cursor = connection.cursor()
 
 cursor.execute("SELECT ip_addr FROM satellites where id = 3")
-
 
 # Variablen in Array speichern
 
@@ -40,8 +39,7 @@ def collector():
     try:
         for satellite in satellite_array:
 
-    cursor = connection.cursor()
-    # Aktuelle geladenes Programm abfragen
+            # Aktuelle geladenes Programm abfragen
     cursor.execute(
         'SELECT current_programm FROM satellites WHERE ip_addr = (%s)', satellit)
     current_programm = cursor.fetchone()
