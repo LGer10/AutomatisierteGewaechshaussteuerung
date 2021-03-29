d5a8113f0561c59ec9eeb5ff289390025099356d
Lars Gerber <lge051198@iet-gibb.ch>
2021-03-29 22:33:33 +0200

    fixed sql fro dateslist dashboard

--- flask_app/app.py
+++ flask_app/app.py
@@ -86,7 +86,7 @@ def dashboard():
         soilhumidity_value_array.append(soilhumidity_v[0])
         soilhumidity_value = soilhumidity_value_array[0]
 
-        cur.execute('''SELECT date FROM sensordata where date >= (SELECT date WHERE id = (%s) and id_satellite_programm in 
+        cur.execute('''SELECT date FROM sensordata where date >= (SELECT date from sensordata WHERE id = (%s) and id_satellite_programm in 
         (select id from satellite_programm where id_satellite = (%s) 
         and id_programm = (%s)))''', [selected_date, satellite_id, programm_id])
         dates = cur.fetchall()
