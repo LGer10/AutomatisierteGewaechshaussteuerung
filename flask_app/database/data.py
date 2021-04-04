db125a2e9cb886e52d792fff1009225bffe7f62a
Lars Gerber <lge051198@iet-gibb.ch>
2021-04-04 14:34:18 +0200

    added comments to database-files

--- flask_app/database/data.py
+++ flask_app/database/data.py
@@ -1,68 +1,69 @@
+#!/usr/bin/python3
+
 # automatisiertes gewächshaus | script - data fro standar programms | version 0.1
 
-#Library für MySQL Verbindung
+# Libraries
 import mysql.connector
 
 
-#Übergabe der Credentials für die Datenbank 
+# MySQL database connection
 db = mysql.connector.connect(
-  host="localhost",
-  database="AGdb",
-  user="root",
-  password="AGdb"
+    host="localhost",
+    database="AGdb",
+    user="root",
+    password="AGdb"
 )
 
-#Cursor zum Ausführen von SQL Befehlen
+# MySQL database cursor
 cursor = db.cursor()
 
-#SQL Befehle
-#Programm1
-cursor.execute('INSERT INTO programms (name) VALUES ("Testprogramm")')
-cursor.execute('INSERT INTO parameters (name, unit) VALUES ("Temperatur", "°C")')
-cursor.execute('INSERT INTO parameters (name, unit) VALUES ("Helligkeit", "h")')
-cursor.execute('INSERT INTO parameters (name, unit) VALUES ("Luftfeuchtigkeit", "°C")')
-cursor.execute('INSERT INTO parameters (name, unit) VALUES ("Bodenfeuchtigkeit", "°C")')
+# create parameters
+cursor.execute(
+    'INSERT INTO parameters (name, unit) VALUES ("Temperatur", "°C")')
+cursor.execute(
+    'INSERT INTO parameters (name, unit) VALUES ("Helligkeit", "h")')
+cursor.execute(
+    'INSERT INTO parameters (name, unit) VALUES ("Luftfeuchtigkeit", "%")')
+cursor.execute(
+    'INSERT INTO parameters (name, unit) VALUES ("Bodenfeuchtigkeit", "%")')
 
-#Parameter
-cursor.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
-VALUES ((select id from programms where name = "Testprogramm"), (select id from parameters where name = "Temperatur"), 25)''') 
-cursor.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
-VALUES ((select id from programms where name = "Testprogramm"), (select id from parameters where name = "Helligkeit"), 8)''')
-cursor.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
-VALUES ((select id from programms where name = "Testprogramm"), (select id from parameters where name = "Luftfeuchtigkeit"), 60)''')
-cursor.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
-VALUES ((select id from programms where name = "Testprogramm"), (select id from parameters where name = "Bodenfeuchtigkeit"), 50)''')
+# create programms
+# chilis
+cursor.execute('INSERT INTO programms (name) VALUES ("Chillis")')
 
-#Programm2
-cursor.execute('INSERT INTO programms (name) VALUES ("Testprogramm1")')
+# chilis-parameter
+cursor.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
+VALUES ((select id from programms where name = "Chillis"), (select id from parameters where name = "Temperatur"), 24)''')
+cursor.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
+VALUES ((select id from programms where name = "Chillis"), (select id from parameters where name = "Helligkeit"), 8)''')
+cursor.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
+VALUES ((select id from programms where name = "Chillis"), (select id from parameters where name = "Luftfeuchtigkeit"), 70)''')
+cursor.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
+VALUES ((select id from programms where name = "Chillis"), (select id from parameters where name = "Bodenfeuchtigkeit"), 50)''')
 
-cursor.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
-VALUES ((select id from programms where name = "Testprogramm1"), (select id from parameters where name = "Temperatur"), 20)''') 
-cursor.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
-VALUES ((select id from programms where name = "Testprogramm1"), (select id from parameters where name = "Helligkeit"), 6)''')
-cursor.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
-VALUES ((select id from programms where name = "Testprogramm1"), (select id from parameters where name = "Luftfeuchtigkeit"), 50)''')
-cursor.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
-VALUES ((select id from programms where name = "Testprogramm1"), (select id from parameters where name = "Bodenfeuchtigkeit"), 40)''')
+# strawberries
+cursor.execute('INSERT INTO programms (name) VALUES ("Erdbeeren")')
 
-cursor.execute('INSERT INTO satellites (name, ip_addr) VALUES ("Testsatellit", "192.168.1.100")')
+# strawberries-parameter
+cursor.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
+VALUES ((select id from programms where name = "Erdbeeren"), (select id from parameters where name = "Temperatur"), 20)''')
+cursor.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
+VALUES ((select id from programms where name = "Erdbeeren"), (select id from parameters where name = "Helligkeit"), 10)''')
+cursor.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
+VALUES ((select id from programms where name = "Erdbeeren"), (select id from parameters where name = "Luftfeuchtigkeit"), 70)''')
+cursor.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
+VALUES ((select id from programms where name = "Erdbeeren"), (select id from parameters where name = "Bodenfeuchtigkeit"), 50)''')
+
+# create satellite
+# prototyp
+cursor.execute(
+    'INSERT INTO satellites (name, ip_addr) VALUES ("PrototypX", "192.168.1.100")')
 cursor.execute('''INSERT INTO satellite_programm(id_programm, id_satellite) 
-VALUES ((select id from programms where name = "Testprogramm"), (select id from satellites where name = "Testsatellit"))''')
+VALUES ((select id from programms where name = "Chillis"), (select id from satellites where name = "PrototypX"))''')
 cursor.execute('''INSERT INTO satellite_programm(id_programm, id_satellite) 
-VALUES ((select id from programms where name = "Testprogramm1"), (select id from satellites where name = "Testsatellit"))''') 
+VALUES ((select id from programms where name = "Erdbeeren"), (select id from satellites where name = "PrototypX"))''')
 
-
-cursor.execute('''insert into sensordata (id_satellite_programm, temperature, brightness, airhumidity, soilhumidity, date, time) 
-VALUES (1, 20, 10, 80, 70, current_date() -4, current_time())''')
-cursor.execute('''insert into sensordata (id_satellite_programm, temperature, brightness, airhumidity, soilhumidity, date, time) 
-VALUES (1, 10, 8, 40, 60, current_date() - 3, current_time())''')
-cursor.execute('''insert into sensordata (id_satellite_programm, temperature, brightness, airhumidity, soilhumidity, date, time) 
-VALUES (1, 20, 6, 60, 40, current_date() - 2, current_time())''')
-cursor.execute('''insert into sensordata (id_satellite_programm, temperature, brightness, airhumidity, soilhumidity, date, time) 
-VALUES (1, 30, 5, 50, 20, current_date() - 1, current_time())''')
-cursor.execute('''insert into sensordata (id_satellite_programm, temperature, brightness, airhumidity, soilhumidity, date, time) 
-VALUES (1, 25, 8, 60, 50, current_date(), current_time())''')
-
+# commit inserts
 db.commit()
+# close cursor
 cursor.close()
-db.close()
