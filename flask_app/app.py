a2fc0a681985219d0213276ebbb36891bd6f1bd3
Lars Gerber <lge051198@iet-gibb.ch>
2021-04-05 13:18:14 +0200

    flash messages

--- flask_app/app.py
+++ flask_app/app.py
@@ -344,7 +344,6 @@ def admin():
                 return render_template('flash.html')
             except:
                 flash('Satellit konnte nicht erstellt werden.')
-                flash('Satellit konnte nicht erstellt werden.')
                 return render_template('flash.html')
 
 
@@ -383,16 +382,16 @@ def admin():
 
             # insert all parameters from user input in input fields
             cur.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
-                        VALUES (%s, (select id from parameters where name = "Temperatur"), %s)''', [programm_id, temperatur])
+                        VALUES (%s, (select id from parameters where name = "Temperatur"), %s)''', [programm_id, temperature])
 
             cur.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
-                        VALUES (%s, (select id from parameters where name = "Helligkeit"), %s)''', [programm_id, helligkeit])
+                        VALUES (%s, (select id from parameters where name = "Helligkeit"), %s)''', [programm_id, brightness])
 
             cur.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
-                        VALUES (%s, (select id from parameters where name = "Luftfeuchtigkeit"), %s)''', [programm_id, luftfeuchtigkeit])
+                        VALUES (%s, (select id from parameters where name = "Luftfeuchtigkeit"), %s)''', [programm_id, airhumidity])
 
             cur.execute('''INSERT INTO programm_parameter(id_programm, id_parameter, value) 
-                        VALUES (%s, (select id from parameters where name = "Bodenfeuchtigkeit"), %s)''', [programm_id, bodenfeuchtigkeit])
+                        VALUES (%s, (select id from parameters where name = "Bodenfeuchtigkeit"), %s)''', [programm_id, soilhumidity])
 
             # commit insert statement
             mysql.connection.commit()
@@ -400,8 +399,15 @@ def admin():
             # close MySQL database cursor
             cur.close()
 
+            flash('Programm wurde erfolgreich erstellt')
+            return render_template('flash.html')
+            except:
+                flash('Programm konnte nicht erstellt werden.')
+                return render_template('flash.html')
+
             # redirect to admin route
             return redirect(url_for('admin'))
+
     # return admin-template with satellites and programm lists
     return render_template('admin.html', satellite_list=satellite_list, programm_list=programm_list)
 
