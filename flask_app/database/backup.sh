32c43bb51e811109a12e99dc4befd85d099e5bdb
Lars Gerber <lge051198@iet-gibb.ch>
2021-04-07 01:53:16 +0200

    add command

--- flask_app/database/backup.sh
+++ flask_app/database/backup.sh
@@ -2,3 +2,7 @@
 
 #command line statement to backup mysql database
 mysqldump -u root -p'AGdb' AGdb > AGdb_backup.sql
+
+#command line statement to delete backups older than 30 days
+find /home/pi/projects/automatisiertes_gewaechshaus/flask_app/database/backups/* -mtime +30 -exec rm {} \;
+
