ed6ad7657544c6b8b09b628a1f8a46aee57b1385
Lars Gerber <lge051198@iet-gibb.ch>
2021-04-07 01:53:08 +0200

    fixed comments

--- flask_app/database/database.py
+++ flask_app/database/database.py
@@ -2,21 +2,21 @@
 
 # automatisiertes gewächshaus | script - create database | version 0.1
 
-# Library für MySQL Verbindung
+# libraries
 import mysql.connector
 
-# MySQL database connection
+# mysql database connection
 db = mysql.connector.connect(
     host="localhost",
     user="root",
     password="AGdb"
 )
 
-# MySQL database cursor
+# mysql database cursor
 cursor = db.cursor()
 
 # create database
 cursor.execute("CREATE DATABASE AGdb")
 
-# MySQL database cursor close
+# mysql database cursor close
 cursor.close()
--- flask_app/database/tables.py
+++ flask_app/database/tables.py
@@ -2,11 +2,11 @@
 
 # automatisiertes gewächshaus | script - create tables | version 0.1
 
-# Libraries
+# libraries
 import mysql.connector
 
 
-# MySQL database connection
+# mysql database connection
 db = mysql.connector.connect(
     host="localhost",
     database="AGdb",
@@ -14,16 +14,15 @@ db = mysql.connector.connect(
     password="AGdb"
 )
 
-# MySQL database cursor
+# mysql database cursor
 cursor = db.cursor()
 
 # create tables
-# Tsatellites
-cursor.execute("CREATE TABLE satellites (id int NOT NULL AUTO_INCREMENT  PRIMARY KEY, name varchar(20), ip_addr varchar(36), current_programm int(2))")
+# satellites
+cursor.execute("CREATE TABLE satellites (id int NOT NULL AUTO_INCREMENT  PRIMARY KEY, name varchar(30), ip_addr varchar(36), current_programm int(2))")
 
 # programms
-cursor.execute(
-    "CREATE TABLE programms (id int NOT NULL AUTO_INCREMENT PRIMARY KEY , name varchar(20), date_created timestamp)")
+cursor.execute("CREATE TABLE programms (id int NOT NULL AUTO_INCREMENT PRIMARY KEY , name varchar(30), date_created timestamp)")
 
 # satellite_programm
 cursor.execute('''CREATE TABLE satellite_programm (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, id_satellite int, id_programm int, FOREIGN KEY (id_satellite)
@@ -34,11 +33,13 @@ cursor.execute('''CREATE TABLE sensordata (id int NOT NULL AUTO_INCREMENT PRIMAR
 brightness float(4), airhumidity float(4), soilhumidity float(4), FOREIGN KEY (id_satellite_programm) REFERENCES satellite_programm(id))''')
 
 # parameters
-cursor.execute(
-    "CREATE TABLE parameters (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(20), unit varchar(5))")
+cursor.execute("CREATE TABLE parameters (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(30), unit varchar(10))")
 
 # programm_parameter
 cursor.execute("CREATE TABLE programm_parameter (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, id_programm int, id_parameter int, value  varchar(4))")
 
-# MySQL database cursor close
+# users
+cursor.execute("CREATE TABLE users (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(30), password varchar(20), email varchar(30), date_created timestamp)")
+
+# mysql database cursor close
 cursor.close()
