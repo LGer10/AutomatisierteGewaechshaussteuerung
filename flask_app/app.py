ed810fe1d19c275bfae339353a3628a7e95d66f8
Lars Gerber <lge051198@iet-gibb.ch>
2021-04-08 01:11:50 +0200

    fix logout

--- flask_app/app.py
+++ flask_app/app.py
@@ -317,9 +317,9 @@ def admin():
                 flash('Login nicht erfolgreich')
                 return render_template('fail.html')
 
-            if request.form['Button'] == 'Logout':
-                    session.pop('user_name', None)
-                    return redirect(url_for('admin'))
+        if request.form['Button'] == 'Logout':
+                session.pop('user_name', None)
+                return redirect(url_for('admin'))
                 
         if request.form['Button'] == 'Programm laden':
             try:
