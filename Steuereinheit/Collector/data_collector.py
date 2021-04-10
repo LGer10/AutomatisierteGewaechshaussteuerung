8fa79fd83e950cda4cd9ba77176ccc0881895078
Lars Gerber <lge051198@iet-gibb.ch>
2021-04-10 14:06:26 +0200

    fixed

--- Steuereinheit/Collector/data_collector.py
+++ Steuereinheit/Collector/data_collector.py
@@ -56,7 +56,7 @@ def collector():
             # convert brightness to hours
             # brightness treshhold is 200
             if brightness >= 200:
-                brightness = 1/60
+                brightness = 0.5/60
             # if brightness below treshhold insert 0 for brightness in database
             else:
                 brightness = 0
