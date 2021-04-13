14ea40881f4829f26d4dbdbb020e8ee1be92d17b
Lars Gerber <lge051198@iet-gibb.ch>
2021-04-13 11:42:38 +0200

    merged

--- test/time_test.py
+++ test/time_test.py
@@ -1,8 +1,14 @@
-import time
+d = {'a': '1'}
 
-running = True
-timeout_start = time.time()
-time_to_shine = 2
+arr.append(array)
 
-while time.time() < timeout_start + time_to_shine:
-    print("test")
+print(arr)
+SELECT brightness FROM sensordata where date >= (SELECT date from sensordata WHERE
+                                                 id=5) and id_satellite_programm in
+(select id from satellite_programm where id_satellite=1
+ and id_programm=1)
+
+
+brightness (1, 2, 3, 4, 5,)
+for item in brightness:
+    print(item)
